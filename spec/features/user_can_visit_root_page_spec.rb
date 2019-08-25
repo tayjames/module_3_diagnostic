6807be47f40a096dfdc048e49a3f94392b2ec0f9
Tay <tay.james.d@gmail.com>
2019-08-25 12:59:01 -0600

    Complete setup for M3 assessment.

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature "User can visit root page" do
   scenario "and view navbar contents" do
     visit "/"
+    
     within(".navbar") do
       expect(page).to have_content("AltFuelFinder")
       expect(page).to have_selector("input[value='Search by zip...']")
