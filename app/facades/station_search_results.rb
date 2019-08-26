e604bf859c5248ff7c332e305f5730ef326ba5e4
Tay <tay.james.d@gmail.com>
2019-08-26 10:50:34 -0600

    Adds conn method in Station facade

--- app/facades/station_search_results.rb
+++ app/facades/station_search_results.rb
@@ -8,8 +8,16 @@ class StationSearchResults
   end
 
   def stations
-    conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json") do |f|
-      faraday.headers["X-API-KEY"] = ENV["API-KEY"]
+    conn = Faraday.new(url: "https://developer.nrel.gov") do |f|
+      f.headers["api_key"] = ENV["API_KEY"]
+      f.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("api/alt-fuel-stations/v1/nearest.json?location=80203&api_key=#{ENV["API_KEY"]}")
+    station_search_data = JSON.parse(response.body, symbolize_names: true)
+
+    station_search_data.map do |station_data|
+      Station.new(station_data)
     end
   end
 end
